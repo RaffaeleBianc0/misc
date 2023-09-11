253ba3c6469cc47e480af82294119c16e3014b98
RaffaeleBianc0 <RaffaeleBianc0@users.noreply.github.com>
2023-09-11 10:51:15 +0200

    Add files via upload

--- rb.psm1
+++ rb.psm1
@@ -10,10 +10,11 @@ INSTALLAZIONE:
         per rendere le funzioni disponibili all'Utente corrente del PC.
 -----------------------------------------------------------------------------
 AUTORE: Raffaele Bianco
+AGGIORNATO IL: 11/9/2023
 =============================================================================
 #>
 
-Function AggiornaProgressbarOBSOLETA {
+function AggiornaProgressbarOBSOLETA {
     <#
         .SYNOPSIS
             Aggiornamento Progressbar con ETA (Estimated Time of Arrival).
@@ -63,7 +64,7 @@ Function AggiornaProgressbarOBSOLETA {
     $Host.UI.RawUI.WindowTitle = "[" + [Math]::Round(($PassNumber / $TotalNumber * 100), 0) + "%] ${ScriptName}"
 }
 
-Function Write-LogAdvancedOBSOLETA {
+function Write-LogAdvancedOBSOLETA {
     <#
         .SYNOPSIS
             Scrive un testo in "formato log" nella console e/o in un LogFile.
@@ -120,7 +121,7 @@ Function Write-LogAdvancedOBSOLETA {
     }
 }
 
-Function Get-FileEncoding($Path) {
+function Get-FileEncoding($Path) {
     $bytes = [byte[]](Get-Content $Path -Encoding byte -ReadCount 4 -TotalCount 4)
 
     if(!$bytes) { return 'utf8' }
@@ -135,7 +136,7 @@ Function Get-FileEncoding($Path) {
     }
 }
 
-Function Out-FileUtf8NoBom {
+function Out-FileUtf8NoBom {
 	<#
     .SYNOPSIS
       Outputs to a UTF-8-encoded file *without a BOM* (byte-order mark).
@@ -215,4 +216,244 @@ Function Out-FileUtf8NoBom {
     finally {
 		$sw.Dispose()
 	}    
+}
+
+function AggiornaProgressbar {
+    <#
+    .SYNOPSIS
+        Aggiornamento Progressbar con ETA (Estimated Time of Arrival).
+    .DESCRIPTION
+        Progressbar avanzata, che visualizza: 
+            - conteggio items processati
+            - conteggio tempo trascorso
+            - stima tempo rimanente
+            - stima orario di termine elaborazione
+    .EXAMPLE
+        if ($itemsProcessed % 100 -eq 0) {AggiornaProgressbar -PassNumber $itemsProcessed -TotalNumber $itemCount -Description $testo}
+    .NOTES
+        Chiamare questa funzione "non troppo spesso" (ad es. ogni 100 $itemsProcessed), per evitare di rallentare troppo lo script.
+    #>
+
+    param(
+        [Parameter(Mandatory = $false)]
+        [int] $PassNumber = 0
+        ,
+        [Parameter(Mandatory = $false)]
+        [int] $TotalNumber = 100
+        ,
+        [Parameter(Mandatory = $false)]
+        [string] $Description = "Attendere..."
+        ,
+        [Parameter(Mandatory = $false)]
+        [int] $Id = 0
+    )
+    $scriptName = Split-Path $PSCommandPath -Leaf
+    $now = Get-Date
+
+    # Tempo trascorso dall'avvio dello script:
+    $timePassed = $now - $Script:StartTimewatch
+
+    # Secondi per elaborare ciascun elemento:
+    $timePer = $timePassed.TotalSeconds / $PassNumber 
+    $itemsPerSecond = ([Math]::Round(($PassNumber / ($timePassed.TotalSeconds + 0.1)), 1))
+    if ($itemsPerSecond -lt 60) {
+        $speed = ($itemsPerSecond * 60).ToString() + "/min"
+    } 
+    else {
+        $speed = $itemsPerSecond.ToString() + "/sec"
+    }
+
+
+    $percentuale = ([Math]::Round(($PassNumber / $TotalNumber * 100), 0)).ToString() + "%"
+    $statusString = $percentuale + "  "
+    $statusString = $statusString + "$PassNumber/$TotalNumber  "
+    $statusString = $statusString + "$($timePassed.Hours.ToString("00")):$($timePassed.Minutes.ToString("00")):$($timePassed.Seconds.ToString("00"))  "
+	if (($PassNumber -ge 3) -and ($TotalNumber -ge 10)) {
+        # Data e ora di completamento stimata:
+        $remainingSeconds = ($TotalNumber - $PassNumber) * $timePer
+        $remainingSpan = New-TimeSpan -seconds $remainingSeconds
+        $eta = $now.AddSeconds($remainingSeconds)
+        if ($now.ToString("d") -eq $eta.ToString("d")) {
+            $FormattedETA = $eta.ToString("H:mm:ss")
+        }
+        else {
+            $FormattedETA = $eta.ToString("d/M/yyyy H:mm:ss")
+        }
+        # $statusString = $statusString + "[-$($remainingSpan.Hours.ToString("00")):$($remainingSpan.Minutes.ToString("00")):$($remainingSpan.Seconds.ToString("00"))]  "
+        $statusString = $statusString + "ETA=$FormattedETA  "
+        $statusString = $statusString + "$speed"
+        $secondiRimanenti = [Math]::Round($remainingSpan.TotalSeconds / 5.0) * 5
+        # Write-Progress -Activity $Description -Status $statusString -PercentComplete ($PassNumber / $TotalNumber * 100) -Id $Id -SecondsRemaining $($remainingSpan.TotalSeconds.ToString())
+        Write-Progress -Activity $Description -Status $statusString -PercentComplete ($PassNumber / $TotalNumber * 100) -Id $Id -SecondsRemaining $secondiRimanenti
+    } else {
+        Write-Progress -Activity $Description -Status $statusString -PercentComplete ($PassNumber / $TotalNumber * 100) -Id $Id
+    }
+    
+    $Host.UI.RawUI.WindowTitle = "[" + $percentuale + "] ${ScriptName}"
+}
+
+function Write-LogAdvanced {
+    <#
+      .SYNOPSIS
+          Scrive un testo in "formato log" nella console e/o in un LogFile.
+      .EXAMPLE
+          Write-Log -Message "Informazione."
+          Write-Log -Message "Errore da registrare nel log definito globalmente a inizio script." -Level "ERROR"
+          Write-Log -Message "Warning da registrare nel log specificato come parametro." -Level "WARN" -LogFile "C:\Logfile.log"
+      .NOTES
+          Aprire il file LOG con VSCode per ottenere lo syntax-highlighting corretto.
+          Per Notepad++: https://darekkay.com/blog/turn-notepad-into-a-log-file-analyzer/
+  #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $True)]
+        [String] $Message
+        ,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("FATAL", "ERROR", "WARN", "INFO", "DEBUG", "OK")]
+        [String] $Level = "INFO"
+        ,
+        [Parameter(Mandatory = $false)]
+        [String] $LogFile = "$(Split-Path $PSCommandPath -Parent)\Log\$(Split-Path $PSCommandPath -Leaf)-$($Script:startTimestamp).log"
+        ,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Console", "Log", "Progressbar", "ConsoleLog", "ConsoleProgressbar", "LogProgressbar", "ConsoleLogProgressbar")]
+        [String] $Mode = "Console"
+        ,
+        [Parameter(Mandatory = $false)]
+        [Int] $CurrentItem
+        ,
+        [Parameter(Mandatory = $false)]
+        [Int] $TotalItems
+        ,
+        [Parameter(Mandatory = $false)]
+        [int] $ProgressbarId = 0
+    )
+
+
+
+    $TimeStamp = (Get-Date).ToString("HH:mm:ss,fff")
+    $Line = "[$TimeStamp $Level] $Message"
+
+
+
+    # CONSOLE:
+    if ($Mode.Contains("Console")) {
+
+        if ($Level -in "ERROR", "FATAL") { 
+            Write-Host "[$TimeStamp $Level]" -BackgroundColor "red" -ForegroundColor "white" -NoNewline
+            if ($Mode.Contains("Progressbar")) { Write-Host " (${CurrentItem}/${TotalItems})" -NoNewLine }
+            Write-Host " $Message"
+        }
+        elseif ($Level -eq "WARN") { 
+            Write-Host "[$TimeStamp $Level]" -BackgroundColor "yellow" -ForegroundColor "black" -NoNewline
+            if ($Mode.Contains("Progressbar")) { Write-Host " (${CurrentItem}/${TotalItems})" -NoNewLine }
+            Write-Host " $Message"
+        }
+        elseif ($Level -eq "DEBUG") { 
+            Write-Host "[$TimeStamp $Level]" -BackgroundColor "cyan" -ForegroundColor "black" -NoNewline
+            if ($Mode.Contains("Progressbar")) { Write-Host " (${CurrentItem}/${TotalItems})" -NoNewLine }
+            Write-Host " $Message"
+        }
+        elseif ($Level -eq "INFO") {
+            Write-Host "[$TimeStamp $Level]" -BackgroundColor "gray" -ForegroundColor "black" -NoNewline
+            if ($Mode.Contains("Progressbar")) { Write-Host " (${CurrentItem}/${TotalItems})" -NoNewLine }
+            Write-Host " $Message"
+        }
+        elseif ($Level -eq "OK") {
+            Write-Host "[$TimeStamp  $Level ]" -BackgroundColor "green" -ForegroundColor "black" -NoNewline
+            if ($Mode.Contains("Progressbar")) { Write-Host " (${CurrentItem}/${TotalItems})" -NoNewLine }
+            Write-Host " $Message"
+        }
+    }
+
+
+
+    # LOG:
+    if ($Mode.Contains("Log")) {
+        $logFolder = $(Split-Path $LogFile -Parent)
+		if (!(Test-Path $logFolder)) { mkdir "$(Split-Path $PSCommandPath -Parent)\Log" | Out-Null }
+		Try {
+			Add-Content $LogFile -Value $Line
+        }
+        Catch {
+            Write-Host "Impossibile scrivere nel file di log ""$LogFile""!" -BackgroundColor "Red" -ForegroundColor "White"
+        }
+    }
+
+
+
+    # PROGRESSBAR:
+    if ($Mode.Contains("Progressbar")) {
+        $CurrentItem = [int]$CurrentItem
+        $TotalItems = [int]$TotalItems
+        AggiornaProgressbar -PassNumber $CurrentItem -TotalNumber $TotalItems -Description $Message -Id $ProgressbarId
+    }
+}
+
+function Test-ValidFileName {
+    param([string]$FileName)
+
+    $indexOfInvalidChar = $FileName.IndexOfAny([System.IO.Path]::GetInvalidFileNameChars())
+
+    # IndexOfAny() returns the value -1 to indicate no such character was found
+    return $indexOfInvalidChar -eq -1
+}
+
+function Test-FolderAccess {
+    param([string]$Folder)
+
+    # Verifico se la $Folder esiste:
+    if (!(Test-Path $Folder)) { return $false }
+
+    # Verifico se posso leggere dalla $Folder un file qualunque (prendo il più piccolo per rapidità di esecuzione):
+    try {
+        # TODO: if ( cartella contiene zero file ) { #     crea file vuoto nella cartella         # }
+        $myFile = Get-ChildItem "$Folder\*" -File | Sort-Object -Property Length | Select-Object -First 1
+        $copiedFile = "$env:Temp\$(Split-Path $myFile -Leaf)"
+        Copy-Item "$myFile" -Destination $copiedFile -Force # | Out-Null
+        if (Test-Path $copiedFile) {
+            Remove-Item -Path "$copiedFile"
+        } else {
+            # Non sono riuscito a scrivere il file nella %TEMP%
+            return $false
+        }
+    }
+    catch {
+        return $false
+    }
+	return $true
+}
+
+function Wait-AllJobs {
+    $runningJobs = Get-Job | Where-Object { $_.State -eq "Running" }
+    $completedJobs = Get-Job | Where-Object { $_.State -eq "Completed" }
+
+    if ($($runningJobs.Count) -le 0) { 
+        $script:fase += $completedJobs.Count # Serve per aggiornare correttamente la progress-bar e il log, nel caso i job siano già completati prima di entrare in questa funzione.
+    }
+
+    $initialJobs = $($runningJobs.Count)
+    $oldCjCount = 0
+    $i = 0
+    while($($runningJobs.Count) -gt 0) {
+        $completedJobs = Get-Job | Where-Object { $_.State -eq "Completed" }
+        $runningJobs = Get-Job | Where-Object { $_.State -eq "Running" }
+
+        $percent = [math]::Round((($initialJobs - $($runningJobs.Count)) / $initialJobs * 100), 0) + ($i++)
+        if ($percent -gt 99) { $percent = 99 }
+        Write-Progress -Activity "Attesa completamento $($runningJobs.Count) Job in esecuzione ($($runningJobs.Name))..." -Status "Completati: $($initialJobs-$($runningJobs.Count))/$initialJobs" -PercentComplete $percent -Id 1 -ParentId 0
+
+        if ($oldCjCount -lt $($completedJobs.Count)) { 
+            $script:fase += ($($completedJobs.Count) - $oldCjCount)
+            Write-LogAdvanced "Job completati: $($completedJobs.Name)" -Level "INFO" -Mode "ConsoleLogProgressbar" -CurrentItem ($script:fase) -TotalItems ($script:totaleFasi)
+            $oldCjCount = $($completedJobs.Count)
+            $i = 0
+        }
+
+        Start-Sleep -Seconds 1
+    }
+    Get-Job | Remove-Job
+    Write-Progress "Job completati." -Id 1 -Completed
 }
